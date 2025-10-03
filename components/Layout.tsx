35ff0a7483a88d704f8b2a6c08e73c6b70d8f322
chan <nch6807@naver.com>
2025-10-03 17:16:41 +0900

    수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,22 +1,35 @@
 // components/Layout.tsx
-import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, use } from "react";
 import { Analytics } from "@vercel/analytics/next";
+import { useRouter, usePathname } from "next/navigation";
+
 
 export default function Layout({ children }: { children: ReactNode }) {
+    const router = useRouter();
+    const pathname = usePathname();
+
+
+    const goBack = () => {
+        if (pathname !== '/') {
+            router.back();
+
+        } else {
+            return;
+        }
+    }
     return (
         <div className="min-h-screen bg-gray-50">
             {/* 헤더 */}
             <header className="w-full p-4 bg-white shadow-sm flex justify-between items-center">
                 <div className="text-lg font-semibold text-gray-700">
-                    본초 2차 수시 화이팅!
+                    본초 수시 화이팅!
                 </div>
-                <Link
-                    href="/"
+                <button
+                    onClick={goBack}
                     className="px-3 py-1 bg-gray-200 hover:bg-gray-300 rounded"
                 >
-                    HOME
-                </Link>
+                    이전으로
+                </button>
             </header>
 
             {/* 본문 */}
